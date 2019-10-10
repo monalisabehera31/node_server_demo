ba514f568dc8c0ed843091cd2c384059861e61f7
monalisa behera <monalisa@monalisas-MacBook-Pro.local>
2019-10-10 10:07:35 +0530

    new branch created

--- server.js
+++
@@ -1,9 +0,0 @@
-const  express = require ('express');
-const app = express();
-app.use(express.static('public'));
-app.get("/", function(req,res){
-    res.send("hello world !");
-});
-
-app.listen(8080);
-console.log('listening on port 8080...');  
